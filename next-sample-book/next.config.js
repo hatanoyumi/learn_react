ba678f26d75c47e29476afd426aa756fd520dc78
Hatano Yumi <>
2024-10-28 15:01:24 +0900

    一旦コミット

---
+++ next-sample-book/next.config.js
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  compiler: (() => {
+    let compilerConfig = {
+      styledComponents: true,
+    }
+
+    if (process.env.NODE_ENV === 'production') {
+      compilerConfig = {
+        ...compilerConfig,
+
+        // 本番環境でReactTestingLibraryで使用するdata-testid属性を削除
+        reactRemoveProperties: { properties: ['^data-testid$'] },
+      }
+    }
+
+    return compilerConfig
+  })(),
+}
+
+module.exports = nextConfig
