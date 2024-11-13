be37ae76b077f1ab656e48e5c0b3387769955fcd
Hatano Yumi <>
2024-11-13 10:29:46 +0900

    修正

--- next-sample-book/jest.config.js
+++ next-sample-book/jest.config.js
@@ -1,9 +1,11 @@
-import nextJest from 'next/jest'
+const nextJest = require('next/jest')
+
 const createJestConfig = nextJest({ dir: './' })
+
 const customJestConfig = {
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  moduleDirectories: ['node_modules', '<rootDir>/src'],
   testEnvironment: 'jsdom',
 }
-export default createJestConfig(customJestConfig)
+
+module.exports = createJestConfig(customJestConfig)
