5842690ab0109a5d662c82ed47274ba31f84de5b
Hatano Yumi <>
2024-12-12 14:54:57 +0900

    nav調整

--- 03_test_tailwindcss/src/components/navbar.tsx
+++ 03_test_tailwindcss/src/components/navbar.tsx
@@ -2,9 +2,9 @@ export default function Navbar() {
   return (
       <nav>
         <ul className="flex text-sm items-center justify-center h-12">
-          <li className="w-36 bg-indigo-200 text-center"><a href="../">index</a></li>
-          <li className="w-36 bg-indigo-200 text-center"><a href="../about">about</a></li>
-          <li className="w-36 bg-indigo-200 text-center"><a href="../testpage">testpage</a></li>
+          <li className="w-36 h-12 bg-indigo-200 mx-1 rounded-lg overflow-hidden"><a href="../" className="flex w-full h-full items-center justify-center">index</a></li>
+          <li className="w-36 h-12 bg-indigo-200 mx-1 rounded-lg overflow-hidden"><a href="../about" className="flex w-full h-full items-center justify-center">about</a></li>
+          <li className="w-36 h-12 bg-indigo-200 mx-1 rounded-lg overflow-hidden"><a href="../testpage" className="flex w-full h-full items-center justify-center">testpage</a></li>
         </ul>
       </nav>
 )
