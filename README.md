36cd7614a0cbb27aa79fe922acfdbcf4d196b163
Hatano Yumi <>
2025-03-14 18:10:19 +0900

    no message

--- README.md
+++ README.md
@@ -8,11 +8,23 @@ ReactとNextの勉強場所
 03 tailwindcss(pages_router)
 
 ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
+learn_js内で勉強
 
 20250123~0213 learn_js → JS学習「これからのjavascriptの教科書」使用
+→ learn_js/Vite/vite-animation
 
 20250213~ ReactでTODOリスト　参照：https://developer.mozilla.org/ja/docs/Learn_web_development/Core/Frameworks_libraries/React_getting_started
+→ learn_js/Vite/React_TODO
 
 2025/2/19〜2/27　React と TypeScript で簡単 TODO アプリ 参照：https://zenn.dev/sprout2000/articles/40328708afaeb9
+→ learn_js/Vite/React_TS_todo
 
-2025/3/12〜　React Hooks と TypeScript でつくる Todo PWA ~ 入門 React ハンズオン　参照：https://zenn.dev/sprout2000/books/76a279bb90c3f3
+2025/3/13~ JavaScript Primer 応用編　参照：https://jsprimer.net/
+3/14 「Ajax通信」
+→ learn_js/JS_Primer/ajaxapp
+「Node.jsでCLIアプリケーション」
+→ learn_js/
+「Todoアプリ」
+→ learn_js/
+
+※保留　2025/3/12〜　React Hooks と TypeScript でつくる Todo PWA ~ 入門 React ハンズオン　参照：https://zenn.dev/sprout2000/books/76a279bb90c3f3
--- learn_js/JS_Primer/ajaxapp/index.js
+++ learn_js/JS_Primer/ajaxapp/index.js
@@ -1,1 +1,109 @@
-console.log("index.js: loaded");
+// const userId = ("js-primer-example");
+
+
+//// Promiseチェーン
+// function main () {
+//   fetchUserInfo("js-primer-example")
+//     // ここではJSONオブジェクトで解決されるPromise
+//     .then((userInfo) => createView(userInfo))
+//     // ここではHTML文字列で解決されるPromise
+//     .then((view) => displayView(view))
+//     // Promiseチェーンでエラーがあった場合はキャッチされる
+//     .catch((error) => {
+//       console.error(`エラーが発生しました (${error})`);
+//     });
+// }
+
+// Async Functionへの置き換え
+async function main() {
+  try {
+    const userId = getUserId();
+    // const userInfo = await fetchUserInfo("js-primer-example");
+    const userInfo = await fetchUserInfo(userId);
+    const view = createView(userInfo);
+    displayView(view);
+  } catch (error) {
+    console.log(`エラーが発生しました (${error})`);
+  }
+}
+
+
+function fetchUserInfo(userId) {
+  // fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
+  // fetchの返り値のPromiseをreturnする
+  return fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
+    .then(response => {
+      if (!response.ok) {
+        // エラーレスポンスからRejectedなPromiseを作成して返す
+        return Promise.reject(new Error(`${response.status}: ${response.statusText}`));
+      } else {
+        // return response.json().then(userInfo => {
+        //   // htmlの組み立て
+        //   // const view = escapeHTML`
+        //   // <h4>${userInfo.name} (@${userInfo.login})</h4>
+        //   // <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
+        //   // <dl>
+        //   //   <dt>Location</dt>
+        //   //   <dd>${userInfo.location}</dd>
+        //   //   <dt>Repositories</dt>
+        //   //   <dd>${userInfo.public_repos}</dd>
+        //   // </dl>
+        //   // `;
+        //   const view = createView(userInfo);
+
+        //   // htmlの挿入
+        //   // const result = document.getElementById("result");
+        //   // result.innerHTML = view;
+        //   displayView(view);
+        // });
+
+        // JSONオブジェクトで解決されるPromiseを返す
+        return response.json();
+      }
+    // })
+    // .catch(err => {
+    //   return Promise.reject(new Error(`failed fetch user(id: ${userId}) info`, { cause: err }));
+    });
+}
+
+function getUserId() {
+  return document.getElementById("userId").value;
+}
+
+function createView(userInfo) {
+  return escapeHTML`
+    <h4>${userInfo.name} (@${userInfo.login})</h4>
+    <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
+    <dl>
+      <dt>Location</dt>
+      <dd>${userInfo.location}</dd>
+      <dt>Repositories</dt>
+      <dd>${userInfo.public_repos}</dd>
+    </dl>
+  `;
+}
+
+function displayView(view) {
+  const result = document.getElementById("result");
+  result.innerHTML = view;
+}
+
+function escapeSpecialChars(str) {
+  return str
+      .replace(/&/g, "&amp;")
+      .replace(/</g, "&lt;")
+      .replace(/>/g, "&gt;")
+      .replace(/"/g, "&quot;")
+      .replace(/'/g, "&#039;");
+}
+
+function escapeHTML(strings, ...values) {
+  return strings.reduce((result, str, i) => {
+    const value =values[i - 1];
+    if (typeof value === "string") {
+      return result + escapeSpecialChars(value) + str;
+    } else {
+      return result + String(value) + str;
+    }
+  });
+}
